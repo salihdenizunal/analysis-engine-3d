668749030e5406903bd22e7dd8ef4cd841412fe0
BatuBALCI <bbalci@larsa4d.com>
2021-06-09 10:47:17 +0300

    Implement constructStructure
    * constructStructure  method which takes (XYZ, supports, connectivity, materials, sections, elementTypes, materialIds, sectionIds, thicknesses) as input and, constructs the node and element objects with structural stiffness and return these is implemented.
    
    * To calculate the global structural stiffness constructGlobalStiffnes is implemented.
    
    * To get the equation numbering of the system getEquationNumbering method is implemented.
    
    * To form structural load vector getLoadVector method is implemented.

---
+++ Structure/getEquaitonNumbering.m
@@ -0,0 +1,23 @@
+function [eqnNumbering, numEqn] = getEquaitonNumbering(numNode, supports)
+    numSupport = size(supports, 1);
+    numDofAtNode = (size(supports, 2)-1);
+    eqnNumbering = zeros(numNode, numDofAtNode);
+    
+    for i = 1:numSupport
+           supportId = supports(i, 1);
+           eqnNumbering(supportId, :) = supports(i, 2:(numDofAtNode+1));
+    end
+    
+    counter = 1;
+    for i = 1:numNode
+        for j = 1:numDofAtNode
+            if(eqnNumbering(i, j) == 0)
+                eqnNumbering(i, j) = counter;
+                counter = counter + 1;
+            else
+                eqnNumbering(i, j) = 0;
+            end
+        end
+    end
+    numEqn = counter - 1;
+end
---
+++ Structure/getLoadVector.m
@@ -0,0 +1,26 @@
+function F = getLoadVector(nodalLoads, nodes)
+numNode = size(nodes,2);
+numLoadedJoints = size(nodalLoads,1);
+% Initiliaze a load matrix which holds the loads on every node of the
+% structure.
+wholeF = zeros(numNode,6);
+
+% Assign the loads on the nodes, respectively.
+count = 1;
+for i = 1:numLoadedJoints
+    for j = 1:6
+        wholeF(nodalLoads(i,1), j) = nodalLoads(i,(j+1));
+    end
+end
+% By using the load matrix of the whole structure create the vector which
+% holds the load on the available degrees of freedoms.
+for i = 1:numNode
+    node = nodes(i);
+    for j = 1:6
+        if(node.globalDofIds(j))
+            F(count,1) = wholeF(i, j);
+            count = count + 1;
+        end
+    end
+end
+end
