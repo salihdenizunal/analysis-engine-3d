b4065b731ff75b4de3d5e8ef5bf56ed8def0bf0c
Belig Batu BALCI <bbalci@larsa4d.com>
2021-09-13 13:11:14 +0300

    Update getInput method and xls file.

--- Input/getInputs.m
+++ Input/getInputs.m
@@ -1,18 +1,30 @@
-function [XYZ, supports, connectivity, materials, sections, thicknesses, elementTypes, materialIds, sectionIds, nodalLoads, deltaL, phi, tolerance, maxIteration, numIncrement, strainType] = getInputs()
+function [XYZ, supports, connectivity, materials, sections, thicknesses, elementTypes, materialIds, sectionIds, nodalLoads, deltaL, phi, tolerance, maxIteration, numIncrement, strainType, initialStartNodeDisp, initialEndNodeDisp] = getInputs()
 
 XYZ = read('Inputs.xlsx', 'Coordinates', 'B2:D3000');
 
 supports = read('Inputs.xlsx', 'Supports', 'B2:H3000');
 
 lineElement = read('Inputs.xlsx', 'Line Elements', 'A2:A2');
+nonlinearLineElement = read('Inputs.xlsx', 'Nonlinear Line Elements', 'A2:A2');
 shellElement = read('Inputs.xlsx', 'Shell Elements', 'A2:A2');
 thicknesses = 0;
 sectionIds = 0;
+strainType = 0;
+initialStartNodeDisp = 0;
+initialEndNodeDisp = 0;
 if(~isempty(lineElement))
     connectivity = read('Inputs.xlsx', 'Line Elements', 'C2:D3000');
     materialIds = read('Inputs.xlsx', 'Line Elements', 'E2:E3000');
     sectionIds = read('Inputs.xlsx', 'Line Elements', 'F2:F3000');
     elementTypes = read('Inputs.xlsx', 'Line Elements', 'B2:B3000');
+elseif(~isempty(nonlinearLineElement))
+    connectivity = read('Inputs.xlsx', 'Nonlinear Line Elements', 'C2:D3000');
+    materialIds = read('Inputs.xlsx', 'Nonlinear Line Elements', 'E2:E3000');
+    sectionIds = read('Inputs.xlsx', 'Nonlinear Line Elements', 'F2:F3000');
+    elementTypes = read('Inputs.xlsx', 'Nonlinear Line Elements', 'B2:B3000');
+    strainType = read('Inputs.xlsx', 'Nonlinear Line Elements', 'S2:S3000');
+    initialStartNodeDisp = read('Inputs.xlsx', 'Nonlinear Line Elements', 'G2:L3000');
+    initialEndNodeDisp = read('Inputs.xlsx', 'Nonlinear Line Elements', 'M2:R3000');
 elseif(~isempty(shellElement))
     connectivity = read('Inputs.xlsx', 'Shell Elements', 'C2:F3000');
     thicknesses = read('Inputs.xlsx', 'Shell Elements', 'G2:G3000');
@@ -37,7 +49,5 @@ tolerance = read('Inputs.xlsx', 'Arc Length Parameters', 'C2:C2');
 maxIteration = read('Inputs.xlsx', 'Arc Length Parameters', 'D2:D2');
 
 numIncrement = read('Inputs.xlsx', 'Arc Length Parameters', 'E2:E2');
-
-strainType = read('Inputs.xlsx', 'Arc Length Parameters', 'F2:F2');
 
 end
