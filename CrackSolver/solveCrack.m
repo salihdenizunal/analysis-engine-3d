dad6bd5a3cea5fc352effbde851957bc44d289f3
Deniz Unal <salihdenizunal@gmail.com>
2022-04-11 21:15:49 +0300

    Add more information and code comments to crack solver
    * Code comments are added.
    
    * Some extra information which are displacement iteration history and curvatures in each iteration are added and returned.

--- CrackSolver/solveCrack.m
+++ CrackSolver/solveCrack.m
@@ -1,36 +1,48 @@
-function [U,force,inertias,moments,internalForces,numberOfIterations] = solveCrack(elements, K, F, n, mIt, tol)
+function [U,force,inertias,moments,internalForces,numberOfIterations,displacementIterations,curvatures] = solveCrack(elements, K, F, n, mIt, tol)
 
+% Initialization of variables.
 numEqn = size(K,1);
 deltaF = (1/n)*F;
 U = zeros(numEqn,n+1);
 numberOfIterations = zeros(1,n+1);
 force = zeros(numEqn,n+1);
 fe = zeros(numEqn,1);
+displacementIterations = zeros(numEqn, 1);
 
+% Hold extra information.
 inertias = zeros(size(elements,2),n+1);
+curvatures = zeros(size(elements,2),n+1,5);
 for elemId = 1: size(elements,2)
     for gaussPoint=1:5
      	inertias(elemId,1,gaussPoint) = elements{elemId}.Ieff(gaussPoint);
     end
+    curvatures(elemId,1,:) = elements{elemId}.calculateCurvature(U);
     moments(elemId,1,:) = elements{elemId}.calculateInternalMoments(U);
     internalForces{elemId,1} = elements{elemId}.calculateInternalForces(U);
 end
 
+% Initialize tangent stiffness.
+Kt = constructGlobalStiffness(elements, numEqn);
+
+% Start increments.
 for i=1:n
-	K = constructGlobalStiffness(elements, numEqn);
-	deltaU = solveForDisplacement(deltaF,K);
+	deltaU = solveForDisplacement(deltaF,Kt);
 	Uc = U(:,i) + deltaU;
 	fe = fe + deltaF;
 	
+    % Update tangent stiffness.
 	for elemId = 1: size(elements,2)
 		element = elements{elemId};
 		element.updateStiffness(Uc);
 	end
+	Kt = constructGlobalStiffness(elements, numEqn);
 	
+    % Start iterations.
 	nIt = 0;
 	while nIt < mIt
 		nIt = nIt + 1;
         
+        % Calculate internal forces.
         fi = zeros(numEqn,1);
         for elemId = 1: size(elements,2)
             elemInternalForces = elements{elemId}.calculateInternalForces(Uc);
@@ -43,17 +55,17 @@ for i=1:n
                 end
             end
         end 
-        
+                
+        % Residual forces.
 		R = fi - fe;
+        
+        % Store history of iterations.
+        displacementIterations(:,end+1) = Uc + solveForDisplacement(-R,Kt);
+
+        % If residual is smaller than tolerance, stop this iteration.
 		if norm(R) < tol
 			break;
-		end
-		
-		for elemId = 1: size(elements,2)
-			element = elements{elemId};
-			element.updateStiffness(Uc);
-		end
-		Kt = constructGlobalStiffness(elements, numEqn);
+        end
 		
 		dellU = solveForDisplacement(-R,Kt);
 		Uc = Uc + dellU;
@@ -63,10 +75,12 @@ for i=1:n
 	force(:,i+1) = fe;
     numberOfIterations(i+1) = nIt;
 
+    % Hold extra information.
 	for elemId = 1: size(elements,2)
         for gaussPoint=1:5
             inertias(elemId,i+1,gaussPoint) = elements{elemId}.Ieff(gaussPoint);
         end
+        curvatures(elemId,i+1,:) = elements{elemId}.calculateCurvature(Uc);
         moments(elemId,i+1,:) = elements{elemId}.calculateInternalMoments(Uc);
         internalForces{elemId,i+1} = elements{elemId}.calculateInternalForces(Uc);
 	end
